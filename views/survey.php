04e205d897133791dacc91aadb3066b1af669dab
Carlos Rubio BM <crubio@bmdigital.co>
2018-05-18 12:57:15 -0500

    Primera parte formulario de encuesta

---
+++ views/survey.php
@@ -0,0 +1,450 @@
+<section class="survey">
+	<div class="container-flex u-color-contraste">
+		<div class="item colum-80">
+			<div class="content-wrapper">
+				<div class="title">
+					<h1 class="animated fadeInLeft">Encuesta de percepción</h1>
+				</div>
+				<div class="formContainer animated fadeInUp">
+					<form>
+						<div class="question">
+							
+							<div class="question-group">
+								
+								<div class="question-header">
+									<div class="question-title">
+										Conferencistas
+									</div>
+									<div class="question-values">
+										<div class="item five">
+											5
+										</div>
+										<div class="item four">
+											4
+										</div>
+										<div class="item three">
+											3
+										</div>
+										<div class="item two">
+											2
+										</div>
+										<div class="item one">
+											1
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Conocimiento y dominio del tema
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="one" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="one" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="one" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="one" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item one">
+											<input type="radio" name="one" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Estimula la participación de los asistentes
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="two" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="two" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="two" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="two" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="two" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Capacidad para comunicar las ideas de forma efectiva
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="three" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="three" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="three" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="three" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="three" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+							</div>
+
+
+							<div class="question-group">
+								
+								<div class="question-header">
+									<div class="question-title">
+										Desarrollo de la temática
+									</div>
+									<div class="question-values">
+										<div class="item five">
+											5
+										</div>
+										<div class="item four">
+											4
+										</div>
+										<div class="item three">
+											3
+										</div>
+										<div class="item two">
+											2
+										</div>
+										<div class="item one">
+											1
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Los temas tratados contribuyen al objetivo general del módulo
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="four" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="four" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="four" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="four" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item one">
+											<input type="radio" name="four" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Si recibió material de apoyo, éste fue de utilidad para el módulo
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="five" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="five" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="five" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="five" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="five" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Metodología utilizada por el docente/conferencista
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="six" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="six" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="six" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="six" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="six" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+							</div>
+
+
+							<div class="question-group">
+								
+								<div class="question-header">
+									<div class="question-title">
+										Logística
+									</div>
+									<div class="question-values">
+										<div class="item five">
+											5
+										</div>
+										<div class="item four">
+											4
+										</div>
+										<div class="item three">
+											3
+										</div>
+										<div class="item two">
+											2
+										</div>
+										<div class="item one">
+											1
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Atención recibida por parte del personal de logística
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="seven" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="seven" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="seven" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="seven" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item one">
+											<input type="radio" name="seven" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Disponibilidad y funcionalidad de los equipos audiovisuales y cómputo
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="eight" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="eight" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="eight" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="eight" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="eight" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Orden y aseo del salón
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="nine" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="nine" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="nine" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="nine" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="nine" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+							</div>
+
+							<div class="question-group">
+								
+								<div class="question-header">
+									<div class="question-title">
+										Monitoría
+									</div>
+									<div class="question-values">
+										<div class="item five">
+											5
+										</div>
+										<div class="item four">
+											4
+										</div>
+										<div class="item three">
+											3
+										</div>
+										<div class="item two">
+											2
+										</div>
+										<div class="item one">
+											1
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Acompañamiento - Respuesta efectiva y atención a  los participantes
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="ten" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="ten" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="ten" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="ten" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item one">
+											<input type="radio" name="ten" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body">
+									<div class="question-label">
+										Actitud de servicio
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five">
+											<input type="radio" name="eleven" value="5">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item four">
+											<input type="radio" name="eleven" value="4">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item three">
+											<input type="radio" name="eleven" value="3">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="eleven" value="2">
+											<span class="checkMark"></span>
+										</div>
+										<div class="item two">
+											<input type="radio" name="eleven" value="1">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+							</div>
+
+							<div class="title">
+								<h4>Generalidades:</h4>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
