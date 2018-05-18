bbc7376cb23b3027aeb935b7a11d029831d73ec5
Carlos R. Casa <crubio@3dm.com.co>
2018-05-18 03:55:11 -0500

    Vista interna de un curso, para el estudiante

---
+++ views/course.php
@@ -0,0 +1,38 @@
+<section class="cursos u-color-contraste">
+	<div class="container-flex">
+		<div class="item colum-60">
+			<div class="content-wrapper">
+				<div class="title">
+					<h1 class="animated fadeInLeft">Nombre del Curos</h1>
+				</div>
+
+				<div class="courseInfo">
+					<div class="description">
+						<h4>Descripción del Curso</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, cum officia maiores cupiditate aperiam vero impedit! Ipsum quibusdam illo vel et quidem minima, natus, officia sint laborum cum, dolorem?.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae magni cum perspiciatis, ipsum pariatur ipsam consequatur laudantium aspernatur, iste doloribus ex, eos porro sequi accusantium! Aliquid distinctio consectetur mollitia, tempora.
+						</p>
+					</div>
+					<div class="date">
+						<h4>Fecha</h4>
+						<p>Mayo 24 - 2018</p>
+					</div>
+					<div class="hours">
+						<h4>Número de horas</h4>
+						<p>200</p>
+					</div>
+					<div class="place">
+						<h4>Luchar</h4>
+						<p>Edificio Fernando Julio salon 308</p>
+					</div>
+
+					<div class="enroll">
+						<button class="btn-square">inscribirme</button>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</div>
+</section>
+
