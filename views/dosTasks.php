5c3e4ab367c1828a2bc12f5d315faaeae5704b54
Carlos Rubio BM <crubio@bmdigital.co>
2018-05-21 17:59:28 -0500

    Archivos vacios

---
+++ views/dosTasks.php
@@ -0,0 +1,12 @@
+<section class="cronograma-dos">
+	<div class="u-color-contraste">
+		<div class="content-wrapper">
+			<div class="title">
+				<h1>Cronograma de actividades</h1>
+			</div>
+			
+
+
+		</div>
+	</div>
+</section>
