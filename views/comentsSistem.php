326c7b77452f1a8682324ccc0af1b5ec7b6cae1c
Carlos Rubio BM <crubio@bmdigital.co>
2018-05-22 11:40:13 -0500

    Vista seguiento a tareas campamento 2, mas sistema de comentarios

--- views/comentsSistem.php
+++ views/comentsSistem.php
@@ -1,3 +1,92 @@
 <div class="coments">
-	
+	<div class="title">
+		<h1 class="text-center">
+			Sistema se seguimiento
+		</h1>
+	</div>
+	<div class="coments-container">
+
+		<div class="coment animated fadeIn">
+			<div class="avatar">
+				<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+			</div>
+			<div class="text">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum voluptate ipsam, hic perferendis repudiandae cupiditate unde voluptatum alias, aut nostrum eos laudantium fugit, inventore neque maxime dolor cum magni iure.
+
+				<div class="date">
+					<small>00/00/00</small> | <small>00:00</small>
+				</div>
+			</div>
+		</div>
+
+		<div class="coment user-loged animated fadeIn">
+			<div class="avatar">
+				<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+			</div>
+			<div class="text">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum voluptate ipsam, hic perferendis repudiandae cupiditate unde voluptatum alias, aut nostrum eos laudantium fugit, inventore neque maxime dolor cum magni iure.
+
+				<div class="date">
+					<small>00/00/00</small> | <small>00:00</small>
+				</div>
+			</div>
+		</div>
+		<div class="coment animated fadeIn">
+			<div class="avatar">
+				<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+			</div>
+			<div class="text">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum voluptate ipsam, hic perferendis repudiandae cupiditate unde voluptatum alias, aut nostrum eos laudantium fugit, inventore neque maxime dolor cum magni iure.
+
+				<div class="date">
+					<small>00/00/00</small> | <small>00:00</small>
+				</div>
+			</div>
+		</div>
+
+		<div class="coment user-loged animated fadeIn">
+			<div class="avatar">
+				<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+			</div>
+			<div class="text">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum voluptate ipsam, hic perferendis repudiandae cupiditate unde voluptatum alias, aut nostrum eos laudantium fugit, inventore neque maxime dolor cum magni iure.
+
+				<div class="date">
+					<small>00/00/00</small> | <small>00:00</small>
+				</div>
+			</div>
+		</div>
+		<div class="coment animated fadeIn">
+			<div class="avatar">
+				<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+			</div>
+			<div class="text">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum voluptate ipsam, hic perferendis repudiandae cupiditate unde voluptatum alias, aut nostrum eos laudantium fugit, inventore neque maxime dolor cum magni iure.
+
+				<div class="date">
+					<small>00/00/00</small> | <small>00:00</small>
+				</div>
+			</div>
+		</div>
+
+		<div class="coment user-loged animated fadeIn">
+			<div class="avatar">
+				<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+			</div>
+			<div class="text">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum voluptate ipsam, hic perferendis repudiandae cupiditate unde voluptatum alias, aut nostrum eos laudantium fugit, inventore neque maxime dolor cum magni iure.
+
+				<div class="date">
+					<small>00/00/00</small> | <small>00:00</small>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<div class="coments-input">
+		<textarea name="" id="" class="form-control" placeholder="Escribe un comentario"></textarea>
+		<button>
+			Enviar
+		</button>
+	</div>
 </div>
--- views/taskFolow.php
+++ views/taskFolow.php
@@ -1,13 +1,60 @@
-<section class="seguimiento-tarea">
+<section class="seguimiento-tarea u-color-contraste">
 	<div class="container-flex">
 		<div class="item colum-60">
 			<div class="content-wrapper">
-				
+				<div class="estados">
+					<div class="fase">
+						Nombre de la fase
+					</div>
+					<div class="estado">
+						Estado de la actividad
+					</div>
+				</div>
+
+				<div class="title">
+					<h1>Nombre de la tarea</h1>
+				</div>
+
+
+				<div class="dates">
+					<div class="date">
+						<small>Fecha de inicio</small>
+						<div>00 | 00 | 00</div>
+					</div>
+					<div class="date-icon">
+
+					</div>
+					<div class="date">
+						<small>Fecha de fin</small>
+						<div>00 | 00 | 00</div>
+					</div>
+				</div>
+
+
+				<div class="form-group">
+					<label for="">
+						Descripción de la actividad
+					</label>
+					<textarea name="tarea" rows="5" class="form-control" placeholder="Descripción" required></textarea>
+				</div>
+
+				<div class="form-group">
+					<label for="">
+						Entregable Asignado
+					</label>
+					<textarea name="tarea" rows="5" class="form-control" placeholder="Descripción del entregable" required></textarea>
+				</div>
+
+				<button class="btn-square">
+					SUBIR ENTREGABLE
+				</button>
 			</div>
 		</div>
 
-		<div class="item colum-40">
-			
+		<div class="item colum-40 contenedorComents">
+			<?php
+				require_once 'views/comentsSistem.php';
+			?>
 		</div>
 	</div>
 </section>
