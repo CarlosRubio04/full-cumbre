abbcd63cc2ab9916e54930070c0db530c8085412
Carlos Rubio BM <crubio@bmdigital.co>
2018-06-05 18:05:42 -0500

    Inicio vista de crear test

---
+++ views/createTest.php
@@ -0,0 +1,28 @@
+<section class="createTest">
+	<div class="container-flex flex-center u-color-contraste u-mount-back">
+		<div class="item colum-60">
+			<div class="content-wrapper">
+				<small>Nombre del curso al que pertenece la prueba</small>
+				<hr>
+				<div class="title">
+					<a href="." class="btn btn-back">
+						<i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i>
+					</a>
+					<h1 class="animated fadeInLeft">
+						Prueba de conocimiento
+					</h1>
+				</div>
+				<div class="formContainer animated fadeInUp">
+					<form method="post">
+						<div class="inputs">
+							<div class="form-group">
+								<label for="">Enunciado</label>
+								<textarea name="" id="" class="form-control" cols="30" rows="10" placeholder="Enunciado de la pregunta"></textarea>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
