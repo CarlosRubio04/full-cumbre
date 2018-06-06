ecd1b158049556ac5cfed970a20cf4b6e56c5bdf
Carlos Rubio BM <crubio@bmdigital.co>
2018-06-06 10:37:09 -0500

    Pantalla calificar entregable, calificar asistencia

---
+++ views/checkAsist.php
@@ -0,0 +1,91 @@
+<section class="assistence">
+	<div class="container-flex flex-center u-color-contraste u-mount-back">
+		<div class="item colum-60">
+			<div class="content-wrapper">
+				<small>Calificar Asistencia del curso BPM</small>
+				<hr>
+				<div class="title">
+					<a href="." class="btn btn-back">
+						<i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i>
+					</a>
+					<h1 class="animated fadeInDown">
+						Nombre del grupo
+					</h1>
+				</div>
+				<hr>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit et, fuga sed temporibus, commodi nostrum mollitia deleniti sit non eveniet nihil necessitatibus, hic aperiam magni iusto quis excepturi provident beatae?
+				</p>
+				<div class="formContainer animated fadeInUp">
+					<form method="post">
+						<div class="inputs">
+							<div class="form-group">								
+								<div class="question-body mt8">
+									<div class="question-label big">
+										<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle"> | Nombre del estudiante
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five big">
+											<input type="checkbox" name="twelve" value="5">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body mt8">
+									<div class="question-label big">
+										<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle"> | Nombre del estudiante
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five big">
+											<input type="checkbox" name="twelve" value="5">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body mt8">
+									<div class="question-label big">
+										<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle"> | Nombre del estudiante
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five big">
+											<input type="checkbox" name="twelve" value="5">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="question-body mt8">
+									<div class="question-label big">
+										<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle"> | Nombre del estudiante
+									</div>	
+
+									<div class="question-inputs">
+										<div class="item five big">
+											<input type="checkbox" name="twelve" value="5">
+											<span class="checkMark"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+
+
+							<!-- Boton Guardar Cuestionario -->
+							<div class="form-group">
+								<button class="btn-square">
+									Calificar asistencia <i class="fa fa-check" aria-hidden="true"></i>
+								</button>
+							</div>
+							<!-- //Boton Guardar Cuestionario -->
+
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- views/productGrade.php
+++ views/productGrade.php
@@ -1,59 +1,67 @@
 <?php if(!defined('directAccess')){ header('location: ../?content=404');}?>
 <section>
 	<form method="post" id="formTask">
-		<div class="content-wrapper  u-color-contraste animated fadeInUp">
-			<div class="addActivityHeader">
-				<div class="name">
-					<h1 class="title">Calificar Entregable</h1>
-				</div>
-			</div>
-			<div class="addActivityBody">
-				<div class="form-group">
-					<label for="">
-						Nombre del Entregable
-					</label>
-					<h3><?php echo $name?></h3>
-				</div>
-				<div class="form-group">
-					<label for="fechaIni">Fecha de entrega</label>
-					<h3><?php echo $startDate?></h3>
-				</div>
-
-				<div class="user form-group" title="Responsable">
-					<label for="">Tareas</label>
-					<div class="form-group">
-						<h3><?php echo $taskHtmlSel?></h3>
+		<div class="container-flex flex-center u-color-contraste u-mount-back">
+			<div class="item colum-60">
+				<div class="content-wrapper animated fadeInUp">
+					<div class="title">
+						<a href="." class="btn btn-back">
+							<i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i>
+						</a>
+						<h1 class="animated fadeInDown">
+							Calificar Entregable
+						</h1>
 					</div>
-				</div>
-				
-				<div class="user form-group" title="Responsable">
-					<label for="">Responsable</label>
-					<div class="form-group">
-						<h3><?php echo $assignedHtmlSel?></h3>
-					</div>
-				</div>
+					<div class="addActivityBody">
+						<div class="form-group">
+							<label for="">
+								Nombre del Entregable
+							</label>
+							<h3><?php echo $name?></h3>
+						</div>
+						<div class="form-group">
+							<label for="fechaIni">Fecha de entrega</label>
+							<h3><?php echo $startDate?></h3>
+						</div>
 
-				<div class="form-group">
-					<label for="">
-						Descripción del entregable
-					</label>
-					<h3><?php echo $descProduct?></h3>
-				</div>
-				<div class="form-group">
-					<label for="">
-						Entregable Guardado:
-					</label>
-					<?php echo $filesHtml?>
-				</div>
-				<div class="form-group">
-					<label for="">
-						Calificación del entregable
-					</label>
-					<input type="number" title="Nota para el Entregable" name="grade" step="0.01" min="0" max="5" class="form-control" required>
-				</div>
-				<h3><?php echo $message?></h3>
-				<div class="form-group">
-					<button type="submit" name="productGrade"  value="1" class="btn btn-sub">Calificar</button>
+						<div class="user form-group" title="Responsable">
+							<label for="">Tareas</label>
+							<div class="form-group">
+								<h3><?php echo $taskHtmlSel?></h3>
+							</div>
+						</div>
+
+						<div class="user form-group" title="Responsable">
+							<label for="">Responsable</label>
+							<div class="form-group">
+								<h3><?php echo $assignedHtmlSel?></h3>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="">
+								Descripción del entregable
+							</label>
+							<h3><?php echo $descProduct?></h3>
+						</div>
+						<div class="form-group">
+							<label for="">
+								Entregable Guardado:
+							</label>
+							<?php echo $filesHtml?>
+						</div>
+						<div class="form-group">
+							<label for="">
+								Calificación del entregable
+							</label>
+							<div class="input">
+								<input type="number" title="Nota para el Entregable" name="grade" placeholder="Nota para el Entregable" step="0.01" min="0" max="5" class="form-control" required>
+								<button type="submit" name="productGrade"  value="1" class="btn btn-sub">Calificar</button>
+							</div>
+							
+						</div>
+						<h3><?php echo $message?></h3>
+					</div>
 				</div>
 			</div>
 		</div>
