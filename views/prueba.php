9879ccb19e563b942b6969f015d8b30723514d5c
Carlos R. Casa <crubio@3dm.com.co>
2018-06-18 05:31:20 -0500

    vista cumbre

---
+++ views/prueba.php
@@ -0,0 +1,77 @@
+<section class="cumbre">
+	<div class="content-wrapper u-mount-back">
+		<div class="title">
+			<a href="." class="btn btn-back">
+				<i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i>
+			</a>
+			<h1 class="animated fadeInDown">
+				Cumbre
+			</h1>
+		</div>
+		<hr>
+		<p class="text-center">
+			Esta es la cumbre de tu proyecto, acá verás las estadisticas de tu rendimiento personal y las de tu equipo.
+		</p>
+
+		<div class="container-flex">
+			<div class="item colum-60">
+				<h4 class="text-center">Estadisticas Personales</h4>
+
+				<div class="User">
+					<div class="UserMts">
+						<p>En total recorriste</p>
+						<div class="data">
+							440 <small>Metros</small>
+						</div>
+					</div>
+					<div class="UserGoals">
+						<p>Estos son los item que lograste</p>
+						<div class="excelencia">
+							<p>Items logrados por excelencia</p>
+							<div class="itemContainer">
+								<div class="item">
+									<img src="img/svg/mapa.svg" alt="mapa">
+									<h4>Mapa</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati aliquam doloribus quibusdam, reprehenderit qui saepe nam non ipsum facilis vel incidunt recusandae perspiciatis, praesentium illum voluptatem molestiae alias quisquam consectetur.</p>
+								</div>
+								<div class="item">
+									<img src="img/svg/botas.svg" alt="mapa">
+									<h4>Botas</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque consequatur nostrum doloremque architecto, distinctio incidunt libero dignissimos necessitatibus voluptatem cupiditate in dolores laborum repellendus sunt alias aspernatur beatae. Veniam, aliquam?</p>
+								</div>
+								<div class="item">
+									<img src="img/svg/brujula.svg" alt="mapa">
+									<h4>Brujula</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea vero, at sit ipsum et accusamus culpa praesentium eius laboriosam repellat rerum error pariatur adipisci veniam id atque esse ratione libero.</p>
+								</div>
+							</div>
+						</div>
+						<div class="logros">
+							<p>Items por cumplimiento</p>
+							<div class="itemContainer">
+								<div class="item">
+									<img src="img/svg/mapa.svg" alt="mapa">
+									<h4>Mapa</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati aliquam doloribus quibusdam, reprehenderit qui saepe nam non ipsum facilis vel incidunt recusandae perspiciatis, praesentium illum voluptatem molestiae alias quisquam consectetur.</p>
+								</div>
+								<div class="item">
+									<img src="img/svg/botas.svg" alt="mapa">
+									<h4>Botas</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque consequatur nostrum doloremque architecto, distinctio incidunt libero dignissimos necessitatibus voluptatem cupiditate in dolores laborum repellendus sunt alias aspernatur beatae. Veniam, aliquam?</p>
+								</div>
+								<div class="item">
+									<img src="img/svg/brujula.svg" alt="mapa">
+									<h4>Brujula</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea vero, at sit ipsum et accusamus culpa praesentium eius laboriosam repellat rerum error pariatur adipisci veniam id atque esse ratione libero.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item colum-60">
+				<h4 class="text-center">Estadisticas Grupales</h4>
+			</div>
+		</div>
+	</div>
+</section>
